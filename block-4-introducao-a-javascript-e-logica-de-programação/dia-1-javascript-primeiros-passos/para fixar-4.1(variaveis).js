ef2d11689faf47515fd821038ebe9bf038cf100b
Juan <juanfernandodemourasilva@gmail.com>
2022-06-20 14:53:55 -0300

    inicio do bloco 4

---
+++ block-4-introducao-a-javascript-e-logica-de-programação/dia-1-javascript-primeiros-passos/para fixar-4.1(variaveis).js
@@ -0,0 +1,9 @@
+let myName = "Juan Fernando";
+let birthCity = "Rio de Janeiro";
+let birthYear = 1998;
+let newBirthYear =2030;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+console.log(newBirthYear);
---
+++ block-4-introducao-a-javascript-e-logica-de-programação/dia-1-javascript-primeiros-passos/para fixar-4.1I(tipagem de variaveis).js
@@ -0,0 +1,18 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+console.log(typeof patientId);
+console.log(typeof patientEmail);
+console.log(typeof isEnrolled);
+
+let base = 5;
+let height = 8;
+let area = base * height;
+let perimeter = area + height;
+
+console.log(area);
+console.log(perimeter);
